656de95ccc04bda572b9bbe0f90c7e8199bfdddb
sowmiya05032006 <sowmiyasowmiya7551@gmail.com>
2026-01-19 15:39:20 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class AttendanceStatusEvaluator{
+  public Static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter an single Positive Integer");
+    int Attendance= sc.nextInt();
+    if(Attendance > 85){
+      System.out.println("Excellent");
+    }
+    else if (Attendance
